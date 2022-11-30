40a991c2cda88a70b13b9d07a2f05183edfe9c42
gldanoob <caleblolhk@gmail.com>
2022-11-30 15:25:32 +0000

    dirname

--- gen.py
+++ gen.py
@@ -5,7 +5,7 @@ with open('template.py', 'r') as f:
     temp = f.read()
 
 for i in range(25):
-    dirname = f'day_{i + 1:02d}'
+    dirname = f'day-{i + 1:02d}'
     if (os.path.isdir(dirname)): continue
     os.mkdir(dirname) 
     with open(f'{dirname}/main.py', 'w') as f:
