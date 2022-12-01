a55e9b8a22fc741b9bb641e1678eb09cbe916007
gldanoob <caleblolhk@gmail.com>
2022-12-01 05:23:54 +0000

    day 1, ima go back to sleep

--- day-01/main.py
+++ day-01/main.py
@@ -1,6 +1,11 @@
 import sys
 
 with open('../' + sys.argv[1]) as f:
+    arr = [0] 
     for line in f.read().splitlines():
-        pass       
-
+        if not line: arr.append(0)
+        else: arr[-1] += int(line)
+        
+    print(max(arr)) 
+    print(sum(sorted(arr)[-3:]))
+   
